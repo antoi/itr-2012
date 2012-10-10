3a3bb080089ba06adec0dc225d83a1f35beca5c4
Antoine Duan <antoine.duan@gmail.com>
2012-10-10 16:42:03 +0800

    Ajoute attribut chef (Employe) methode dirChef()

--- Employe.java
+++ Employe.java
@@ -9,14 +9,19 @@
 public class Employe extends Personne {
 
 	/**
-	 * numŽro d'employŽ
+	 * numå·–o d'employï¿½
 	 */
 	private int numero;
 	
 	/**
-	 * Entreprise dans laquelle travaille l'employŽ
+	 * Entreprise dans laquelle travaille l'employï¿½
 	 */
 	Entreprise entreprise;
+	
+	/**
+	 * le chef 
+	 */
+	private Employe chef;
 	
 	/**
 	 * @return the entreprise
@@ -56,9 +61,27 @@ public class Employe extends Personne {
 		super(nom, prenom, sexe, anneeNaissance);
 		this.numero = numero;
 		this.entreprise = entreprise;
+		this.chef = null;
 	}
 	
 	
+	/**
+	 * @param nom
+	 * @param prenom
+	 * @param sexe
+	 * @param anneeNaissance
+	 * @param numero
+	 * @param entreprise
+	 * @param chef
+	 */
+	public Employe(String nom, String prenom, boolean sexe, int anneeNaissance,
+			int numero, Entreprise entreprise, Employe chef) {
+		super(nom, prenom, sexe, anneeNaissance);
+		this.numero = numero;
+		this.entreprise = entreprise;
+		this.chef = chef;
+	}
+
 	/* (non-Javadoc)
 	 * @see Personne#toString()
 	 */
@@ -66,17 +89,43 @@ public class Employe extends Personne {
 	public String toString() {
 		// TODO Auto-generated method stub
 		return super.toString()+" - travaille chez "+entreprise.getNom()+
-		", numŽro "+this.numero;
+		", numå·–o "+this.numero;
 	}
 
 	public static void main(String[] args) {
 		Entreprise comac = new Entreprise("Comac");
-		Employe e1 = new Employe("Dassault","Marcel",true,1910,354,comac);
-		Employe e2 = new Employe("Sarkozy","Nicolas",true,1940,335, comac);
+		Manager e0 = new Manager("Mao", "Zedong", true, 1893, 1976, comac, null);
+		Employe e1 = new Employe("Dassault","Marcel",true,1910,354,comac,e0);
+		Employe e2 = new Employe("Sarkozy","Nicolas",true,1940,335, comac, e0);
 		
 		System.out.println(e1);
 		System.out.println(e2);
 		
+		e1.dirChef();
+		e2.dirChef();
+		e0.dirChef();
+	}
+
+	/**
+	 * @return the chef
+	 */
+	public Employe getChef() {
+		return chef;
+	}
+
+	/**
+	 * @param chef the chef to set
+	 */
+	public void setChef(Employe chef) {
+		this.chef = chef;
+	}
+	
+	public void dirChef(){
+		if (this.chef == null){
+			System.out.println(this.getNom() + " est le patron");
+		} else {
+			System.out.println( this.getNom() + " a pour chef " + this.chef.getNom());
+		}
 	}
 
 }
